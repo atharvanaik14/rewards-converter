4e5521ae5af1ea7cdc4e36c6ea761c8848c53453
atharvanaik14 <atharva.naik20@vit.edu>
2023-10-14 22:44:44 +0530

    Added function to convert Rewards

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,28 @@
+public class RewardValue {
+    private final double cash;
+
+    RewardValue(double cash) {
+        this.cash = cash;
+    }
+
+    RewardValue(int miles) {
+        this.cash = convertCashValue(miles);
+    }
+
+
+    public int convertCashValue(double cash) {
+        return (int) (cash / 0.0035); 
+    }
+
+    public double convertMilesValue(int miles) {
+        return miles * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cash;
+    }
+
+    public int getMilesValue() {
+        return convertCashValue(this.cash);
+    }
+}
